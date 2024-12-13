526521ce2d312ee5a94838ce23130e86cbd6c6d5
sjbremod <sjbremod@gmail.com>
2024-12-12 22:14:27 -0500

    custom methods, importing, type promoting, var args

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -39,4 +39,54 @@ public class ArrayUtil {
 
         return result;
     }
+
+    /**
+     * this method returns the index of given int num from the give int array
+     * @param arr given int array
+     * @param num given int value
+     * @return the index of value if exists, if not returns -1
+     */
+    public static int indexOf (int []arr , int num){
+        int index = -1;
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] == num){
+                index = i;
+                break;
+            }
+        }
+        return index;
+
+    }
+    /**
+     * this method returns the index of given String str from the give String array
+     * @param arr given string array
+     * @param str given string value
+     * @return the index of value if exists, if not returns -1
+     */
+    public static int indexOf (String []arr , String  str){
+        int index = -1;
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] .equals(str)){
+                index = i;
+                break;
+            }
+        }
+        return index;
+
+    }
+
+    /**
+     * this method returns the sum of all the int numbers in the given int array
+     * @param arr given int array
+     * @return the total sum of all elements
+     */
+    public static int sum(int ... arr){
+        int total = 0;
+        for (int each  :arr  ){
+            total += each;
+        }
+
+
+        return total;
+    }
 }
